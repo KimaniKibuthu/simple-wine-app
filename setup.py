6adfd277429a5ffbb269d08552d8282745eae6ca
KimaniKibuthu <32309808+KimaniKibuthu@users.noreply.github.com>
2023-02-08 22:56:33 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Creation of a simple wine app for fun as I practice the end to end ML pipeline flow.',
+    author='Kimani Kibuthu',
+    license='MIT',
+)
