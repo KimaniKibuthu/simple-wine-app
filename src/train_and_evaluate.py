99373a337aa416d4175dfc6e62595729c2e1bdb8
KimaniKibuthu <32309808+KimaniKibuthu@users.noreply.github.com>
2023-02-09 22:34:49 +0300

    updated train_and_evaluate.py

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1,9 +1,18 @@
 import os
 import argparse
+import json
+import joblib
 import pandas as pd
 from sklearn.linear_model import ElasticNet
 from sklearn import metrics
 from get_data import read_params
+
+
+def eval_metrics(actual, pred):
+    rmse = metrics.mean_squared_error(actual, pred, squared=False)
+    mae = metrics.mean_absolute_error(actual, pred)
+    r2 = metrics.r2_score(actual, pred)
+    return rmse, mae, r2
 
 def train_and_evaluate(config_path):
     # Get data
@@ -11,6 +20,7 @@ def train_and_evaluate(config_path):
     train_path = config["split_data"]["train_path"]
     test_path =config["split_data"]["test_path"]
     target = config["base"]["target_col"]
+    model_dir = config["model_dir"]
     alpha = config["estimators"]["ElasticNet"]["params"]["alpha"]
     l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
 
@@ -28,9 +38,36 @@ def train_and_evaluate(config_path):
     elastic = ElasticNet(l1_ratio=l1_ratio, alpha=alpha)
     elastic.fit(x_train, y_train)
     preds = elastic.predict(x_test)
-    mae = metrics.mean_absolute_error(y_test, preds)
-    print(mae)
+    rmse, mae, r2 = eval_metrics(y_test, preds)
 
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+
+    scores_file = config["reports"]["scores"]
+    params_file = config["reports"]["params"]
+
+    with open(scores_file, "w") as f:
+        scores = {
+            "rmse": rmse,
+            "mae": mae,
+            "r2": r2
+        }
+        json.dump(scores, f, indent=4)
+
+    with open(params_file, "w") as f:
+        params = {
+            "alpha": alpha,
+            "l1_ratio": l1_ratio,
+        }
+        json.dump(params, f, indent=4)
+
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
+
+    joblib.dump(elastic, model_path)
+    
     # Save model and accuracy
 
 
